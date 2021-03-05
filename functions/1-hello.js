126ac7f3d2522dffb5e197e06e7b507e0ac3daf3
Fede <fede.fede.website>
2021-03-05 14:19:38 -0600

    usinng callback instead of async

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,7 +1,11 @@
 //domain/.netlify/functions/1-hello
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: 'Our First Netlify Function Example',
-    }
+exports.handler = (event, context, cb) => {
+
+    cb(null,
+        {statusCode: 200, body: 'Hello world!'})
+
+    //return {
+    //    statusCode: 200,
+    //    body: 'Our First Netlify Function Example',
+    //}
 }
