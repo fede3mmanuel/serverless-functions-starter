ff66f7904881b1cbe2757984fc03bad2b55c93c2
Fede <fede.fede.website>
2021-04-02 12:12:39 -0600

    add map function

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -3,6 +3,9 @@ const result = document.querySelector('.result')
 const fetchData = async () => {
     try {
         const {data} = await axios.get('/api/2-basic-api');
+        const products = data.map((product)=>{
+            console.log(product);
+        })
         result.innerHTML = `<h2>Success</h2>`
     } catch (error) {
         result.innerHTML = `<h4>There was an error. Please try again later</h4>`
