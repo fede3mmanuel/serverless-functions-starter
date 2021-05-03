29f3434f355f256955b6e7dcd13d3aa09314ef8a
Fede <fede.fede.website>
2021-05-03 17:02:21 -0500

    Added airtable api

--- functions/3-airtable.js
+++ functions/3-airtable.js
@@ -1,13 +1,31 @@
+require('dotenv').config()
 const Airtable = require('airtable-node');
  
-const airtable = new Airtable({ apiKey: 'xxx' })
-  .base('appRvT3sM3XOBruUk')
-  .table('Team Members')
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('app1jnfDtvHiW6LPI')
+  .table('products')
  
 
 exports.handler = async (event, context, cb) => {
-    return {
-        statusCode: 200,
-        body: 'Airtable Function',
+    try {
+        const { records } = await airtable.list()
+        const products = records.map((product) => {
+            const { id } = product
+            const { name, image, price } = product.fields
+            const url = image[0].url
+            // console.log(image)
+            // return {id,name,url,price}
+            return {id, name, url, price};
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products),
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Server error',
+        }
     }
+    
 }
